34dd5316d657cca56d64530813437ca0e18838dc
Derek Gifford <derekisbusy@gmail.com>
2017-01-18 07:53:12 -0500

    Added getStates() method

--- models/GeoState.php
+++ models/GeoState.php
@@ -23,4 +23,12 @@ class GeoState extends BaseGeoState
         ]);
     }
 	
+    public static function getStates() {
+        $states = self::find()->asArray()->all();
+        $state_ids[0] = "";
+        foreach($states as $state) {
+            $state_ids[$state['id']] = $state['state_code'];
+        }
+        return $state_ids;
+    }
 }
