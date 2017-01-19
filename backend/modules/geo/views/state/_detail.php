f66a611966c72761caf60013966f08a2a845acc0
Derek Gifford <derekisbusy@gmail.com>
2017-01-19 10:42:33 -0500

    Add state _detail view

---
+++ backend/modules/geo/views/state/_detail.php
@@ -0,0 +1,32 @@
+<?php
+
+use derekisbusy\geo\models\GeoState;
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model GeoState */
+
+?>
+<div class="geo-state-view">
+
+    <div class="row">
+        <div class="col-sm-9">
+            <h2><?= Html::encode($model->state) ?></h2>
+        </div>
+    </div>
+
+    <div class="row">
+<?php 
+    $gridColumn = [
+        ['attribute' => 'id', 'visible' => false],
+        'state',
+        'state_code',
+    ];
+    echo DetailView::widget([
+        'model' => $model,
+        'attributes' => $gridColumn
+    ]); 
+?>
+    </div>
+</div>
