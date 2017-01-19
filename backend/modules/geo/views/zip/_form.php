bf9c0ad805d865644510b9717c95dcd492023382
Derek Gifford <derekisbusy@gmail.com>
2017-01-19 11:09:28 -0500

    Add zip _form view

---
+++ backend/modules/geo/views/zip/_form.php
@@ -0,0 +1,56 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model vendor\derekisbusy\geo\models\GeoZip */
+/* @var $form yii\widgets\ActiveForm */
+
+?>
+
+<div class="geo-zip-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->errorSummary($model); ?>
+
+    <?= $form->field($model, 'id', ['template' => '{input}'])->textInput(['style' => 'display:none']); ?>
+
+    <?= $form->field($model, 'zip')->textInput(['maxlength' => true, 'placeholder' => 'Zip']) ?>
+
+    <?= $form->field($model, 'latitude')->textInput(['placeholder' => 'Latitude']) ?>
+
+    <?= $form->field($model, 'longitude')->textInput(['placeholder' => 'Longitude']) ?>
+
+    <?= $form->field($model, 'county_id')->widget(\kartik\widgets\Select2::classname(), [
+        'data' => \yii\helpers\ArrayHelper::map(\vendor\derekisbusy\geo\models\GeoCounty::find()->orderBy('id')->asArray()->all(), 'id', 'id'),
+        'options' => ['placeholder' => Yii::t('derekisbusy/geo', 'Choose Geo county')],
+        'pluginOptions' => [
+            'allowClear' => true
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'state_id')->widget(\kartik\widgets\Select2::classname(), [
+        'data' => \yii\helpers\ArrayHelper::map(\vendor\derekisbusy\geo\models\GeoState::find()->orderBy('id')->asArray()->all(), 'id', 'id'),
+        'options' => ['placeholder' => Yii::t('derekisbusy/geo', 'Choose Geo state')],
+        'pluginOptions' => [
+            'allowClear' => true
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'city_id')->widget(\kartik\widgets\Select2::classname(), [
+        'data' => \yii\helpers\ArrayHelper::map(\vendor\derekisbusy\geo\models\GeoCity::find()->orderBy('id')->asArray()->all(), 'id', 'id'),
+        'options' => ['placeholder' => Yii::t('derekisbusy/geo', 'Choose Geo city')],
+        'pluginOptions' => [
+            'allowClear' => true
+        ],
+    ]); ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('derekisbusy/geo', 'Create') : Yii::t('derekisbusy/geo', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
