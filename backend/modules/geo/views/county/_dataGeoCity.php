10e86f74e195c44038f6e481ceb6ca586759034d
Derek Gifford <derekisbusy@gmail.com>
2017-01-19 11:02:32 -0500

    Update county views

--- backend/modules/geo/views/county/_dataGeoCity.php
+++ backend/modules/geo/views/county/_dataGeoCity.php
@@ -2,42 +2,42 @@
 use kartik\grid\GridView;
 use yii\data\ArrayDataProvider;
 
-    $dataProvider = new ArrayDataProvider([
-        'allModels' => $model->geoCities,
-        'key' => 'id'
-    ]);
-    $gridColumns = [
-        ['class' => 'yii\grid\SerialColumn'],
-        ['attribute' => 'id', 'visible' => false],
-        'city',
+$dataProvider = new ArrayDataProvider([
+    'allModels' => $model->geoCities,
+    'key' => 'id'
+]);
+$gridColumns = [
+    ['class' => 'yii\grid\SerialColumn'],
+    ['attribute' => 'id', 'visible' => false],
+    'city',
+    [
+            'attribute' => 'state.id',
+            'label' => Yii::t('geo', 'State')
+        ],
+    [
+        'class' => 'yii\grid\ActionColumn',
+        'controller' => 'geo-city'
+    ],
+];
+
+echo GridView::widget([
+    'dataProvider' => $dataProvider,
+    'columns' => $gridColumns,
+    'containerOptions' => ['style' => 'overflow: auto'],
+    'pjax' => true,
+    'beforeHeader' => [
         [
-                'attribute' => 'state.id',
-                'label' => Yii::t('geo', 'State')
-            ],
-        [
-            'class' => 'yii\grid\ActionColumn',
-            'controller' => 'geo-city'
-        ],
-    ];
-    
-    echo GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => $gridColumns,
-        'containerOptions' => ['style' => 'overflow: auto'],
-        'pjax' => true,
-        'beforeHeader' => [
-            [
-                'options' => ['class' => 'skip-export']
-            ]
-        ],
-        'export' => [
-            'fontAwesome' => true
-        ],
-        'bordered' => true,
-        'striped' => true,
-        'condensed' => true,
-        'responsive' => true,
-        'hover' => true,
-        'showPageSummary' => false,
-        'persistResize' => false,
-    ]);
+            'options' => ['class' => 'skip-export']
+        ]
+    ],
+    'export' => [
+        'fontAwesome' => true
+    ],
+    'bordered' => true,
+    'striped' => true,
+    'condensed' => true,
+    'responsive' => true,
+    'hover' => true,
+    'showPageSummary' => false,
+    'persistResize' => false,
+]);
