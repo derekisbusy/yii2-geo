e8f8872c7f22b83eb73721f3f2f6423e570cc0ed
Derek Gifford <derekisbusy@gmail.com>
2017-01-19 11:01:37 -0500

    Add county _dataGeoZip view

---
+++ backend/modules/geo/views/county/_dataGeoZip.php
@@ -0,0 +1,49 @@
+<?php
+use kartik\grid\GridView;
+use yii\data\ArrayDataProvider;
+
+    $dataProvider = new ArrayDataProvider([
+        'allModels' => $model->geoZips,
+        'key' => 'id'
+    ]);
+    $gridColumns = [
+        ['class' => 'yii\grid\SerialColumn'],
+        ['attribute' => 'id', 'visible' => false],
+        'zip',
+        'latitude',
+        'longitude',
+        [
+                'attribute' => 'state.id',
+                'label' => Yii::t('geo', 'State')
+            ],
+        [
+                'attribute' => 'city.id',
+                'label' => Yii::t('geo', 'City')
+            ],
+        [
+            'class' => 'yii\grid\ActionColumn',
+            'controller' => 'geo-zip'
+        ],
+    ];
+    
+    echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => $gridColumns,
+        'containerOptions' => ['style' => 'overflow: auto'],
+        'pjax' => true,
+        'beforeHeader' => [
+            [
+                'options' => ['class' => 'skip-export']
+            ]
+        ],
+        'export' => [
+            'fontAwesome' => true
+        ],
+        'bordered' => true,
+        'striped' => true,
+        'condensed' => true,
+        'responsive' => true,
+        'hover' => true,
+        'showPageSummary' => false,
+        'persistResize' => false,
+    ]);
