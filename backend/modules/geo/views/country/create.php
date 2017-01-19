e8d30cd2c99b1402e0e1ddaf5c0438861803f0a0
Derek Gifford <derekisbusy@gmail.com>
2017-01-19 10:48:12 -0500

    Add country create view

---
+++ backend/modules/geo/views/country/create.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model derekisbusy\geo\models\GeoCountry */
+
+$this->title = Yii::t('geo', 'Create Country');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('geo', 'Countries'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="geo-country-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
