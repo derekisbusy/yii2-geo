2bdec8b5bbb2a1f6ec2df5454d38bb04035b4a22
Derek Gifford <derekisbusy@gmail.com>
2017-01-19 10:43:48 -0500

    Add state _dataGeoCity view

---
+++ backend/modules/geo/views/state/_dataGeoCity.php
@@ -0,0 +1,43 @@
+<?php
+use kartik\grid\GridView;
+use yii\data\ArrayDataProvider;
+
+    $dataProvider = new ArrayDataProvider([
+        'allModels' => $model->geoCities,
+        'key' => 'id'
+    ]);
+    $gridColumns = [
+        ['class' => 'yii\grid\SerialColumn'],
+        ['attribute' => 'id', 'visible' => false],
+        'city',
+        [
+                'attribute' => 'county.id',
+                'label' => Yii::t('geo', 'County')
+            ],
+        [
+            'class' => 'yii\grid\ActionColumn',
+            'controller' => 'geo-city'
+        ],
+    ];
+    
+    echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => $gridColumns,
+        'containerOptions' => ['style' => 'overflow: auto'],
+        'pjax' => true,
+        'beforeHeader' => [
+            [
+                'options' => ['class' => 'skip-export']
+            ]
+        ],
+        'export' => [
+            'fontAwesome' => true
+        ],
+        'bordered' => true,
+        'striped' => true,
+        'condensed' => true,
+        'responsive' => true,
+        'hover' => true,
+        'showPageSummary' => false,
+        'persistResize' => false,
+    ]);
