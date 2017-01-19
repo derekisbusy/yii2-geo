06083c0d20df430b6ca74adaa3e6789c42a443ca
Derek Gifford <derekisbusy@gmail.com>
2017-01-19 10:41:51 -0500

    Add state _expand view

---
+++ backend/modules/geo/views/state/_expand.php
@@ -0,0 +1,45 @@
+<?php
+use yii\helpers\Html;
+use kartik\tabs\TabsX;
+use yii\helpers\Url;
+$items = [
+    [
+        'label' => '<i class="glyphicon glyphicon-book"></i> '. Html::encode(Yii::t('geo', 'State')),
+        'content' => $this->render('_detail', [
+            'model' => $model,
+        ]),
+    ],
+        [
+        'label' => '<i class="glyphicon glyphicon-book"></i> '. Html::encode(Yii::t('geo', 'City')),
+        'content' => $this->render('_dataGeoCity', [
+            'model' => $model,
+            'row' => $model->geoCities,
+        ]),
+    ],
+            [
+        'label' => '<i class="glyphicon glyphicon-book"></i> '. Html::encode(Yii::t('geo', 'County')),
+        'content' => $this->render('_dataGeoCounty', [
+            'model' => $model,
+            'row' => $model->geoCounties,
+        ]),
+    ],
+            [
+        'label' => '<i class="glyphicon glyphicon-book"></i> '. Html::encode(Yii::t('geo', 'Zip')),
+        'content' => $this->render('_dataGeoZip', [
+            'model' => $model,
+            'row' => $model->geoZips,
+        ]),
+    ],
+    ];
+echo TabsX::widget([
+    'items' => $items,
+    'position' => TabsX::POS_ABOVE,
+    'encodeLabels' => false,
+    'class' => 'tes',
+    'pluginOptions' => [
+        'bordered' => true,
+        'sideways' => true,
+        'enableCache' => false
+    ],
+]);
+?>
