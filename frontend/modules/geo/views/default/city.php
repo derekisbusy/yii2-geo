400e80c5558677f532e439d901227497deaf1c19
Derek Gifford <derekisbusy@gmail.com>
2017-01-31 18:06:57 -0500

    Minor updates.

--- frontend/modules/geo/views/default/city.php
+++ frontend/modules/geo/views/default/city.php
@@ -1,9 +1,11 @@
 <?php
 
-$this->title="{$city->city}, {$city->state_code}";
+$this->title="{$city->city}, {$city->state->state_code}";
 
 
 ?>
 
 <div id="page-city">
-<h2><?php echo $city->city.', '.$city->state_code?></h2>
+<h2><?php echo $city->city.', '.$city->state->state_code?></h2>
+
+</div>
