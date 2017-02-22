22f6d6ea505d49430e5e6f487d9b55114946a1b0
Derek Gifford <derekisbusy@gmail.com>
2017-02-22 16:29:44 -0500

    Add `actionState()` method. Update namespacing.

--- frontend/modules/geo/controllers/DefaultController.php
+++ frontend/modules/geo/controllers/DefaultController.php
@@ -3,9 +3,12 @@
 namespace derekisbusy\geo\frontend\modules\geo\controllers;
 
 use derekisbusy\geo\models\GeoCity;
-use derekisbusy\geo\models\GeoCitySearch;
+use derekisbusy\geo\models\GeoCityAliasSearch;
 use derekisbusy\geo\models\GeoCounty;
+use derekisbusy\geo\models\GeoState;
+use derekisbusy\geo\models\GeoStateSearch;
 use yii\web\Controller;
+use Yii;
 
 /**
  * Default controller for the `geo` module
@@ -14,10 +17,23 @@ class DefaultController extends Controller
 {
     public function actionIndex()
     {
-        $searchModel = new GeoCitySearch;
-        $searchModel->state_id = 10;
+        $searchModel = new GeoStateSearch;
         $dataProvider = $searchModel->search(\Yii::$app->request->getQueryParams());
         return $this->render('index',['dataProvider'=>$dataProvider,'searchModel'=>$searchModel]);
+    }
+    
+    public function actionState($stateCode, $stateName)
+    {
+        $stateName = str_replace('_',' ',$stateName);
+        $state = GeoState::find()->where(['LIKE','state', $stateName])->andWhere(['state_code' => $stateCode])->one();
+        if ($state === null) {
+            throw new \yii\web\NotFoundHttpException(Yii::t('common', 'The page could not be found.'));
+        }
+        $searchModel = new GeoCityAliasSearch;
+        $searchModel->state_id = $state->id;
+        $request = \Yii::$app->request;
+        $dataProvider = $searchModel->search($request->getQueryParams());
+        return $this->render('state',['dataProvider'=>$dataProvider,'searchModel'=>$searchModel, 'state' => $state]);
     }
     
     public function actionCity($cityName)
