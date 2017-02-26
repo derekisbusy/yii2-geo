9bff0a7e57f1e10cd52bced71247323c435eb46f
Derek Gifford <derekisbusy@gmail.com>
2017-02-26 15:27:05 -0500

    Update zip forms.

--- backend/modules/geo/views/zip/_form.php
+++ backend/modules/geo/views/zip/_form.php
@@ -1,10 +1,16 @@
 <?php
 
+use derekisbusy\geo\models\GeoCity;
+use derekisbusy\geo\models\GeoCounty;
+use derekisbusy\geo\models\GeoState;
+use derekisbusy\geo\models\GeoZip;
 use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\web\JsExpression;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model derekisbusy\geo\models\GeoZip */
+/* @var $model GeoZip */
 /* @var $form yii\widgets\ActiveForm */
 
 ?>
@@ -23,27 +29,51 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'longitude')->textInput(['placeholder' => 'Longitude']) ?>
 
-    <?= $form->field($model, 'county_id')->widget(\kartik\widgets\Select2::classname(), [
-        'data' => \yii\helpers\ArrayHelper::map(\derekisbusy\geo\models\GeoCounty::find()->orderBy('id')->asArray()->all(), 'id', 'id'),
-        'options' => ['placeholder' => Yii::t('geo', 'Choose county')],
-        'pluginOptions' => [
-            'allowClear' => true
-        ],
-    ]); ?>
 
-    <?= $form->field($model, 'state_id')->widget(\kartik\widgets\Select2::classname(), [
-        'data' => \yii\helpers\ArrayHelper::map(\derekisbusy\geo\models\GeoState::find()->orderBy('id')->asArray()->all(), 'id', 'id'),
-        'options' => ['placeholder' => Yii::t('geo', 'Choose state')],
-        'pluginOptions' => [
-            'allowClear' => true
-        ],
-    ]); ?>
+    <?php
+//    echo $form->field($model, 'state_id')->widget(\kartik\widgets\Select2::classname(), [
+//        'data' => \yii\helpers\ArrayHelper::map(GeoState::find()->orderBy('id')->asArray()->all(), 'id', 'state'),
+//        'options' => ['placeholder' => Yii::t('geo', 'Choose state')],
+//        'pluginOptions' => [
+//            'allowClear' => true
+//        ],
+//    ]); ?>
+    
+    <?php
+//    echo $form->field($model, 'county_id')->widget(\kartik\widgets\Select2::classname(), [
+////        'data' => \yii\helpers\ArrayHelper::map(\derekisbusy\geo\models\GeoCounty::find()->orderBy('id')->asArray()->all(), 'id', 'id'),
+//        'options' => ['placeholder' => Yii::t('geo', 'Choose county')],
+//        'initValueText' =>  $model->county ? GeoCounty::findOne($model->county_id)->county : '' ,
+//        'pluginOptions' => [
+//            'allowClear' => true,
+//            'minimumInputLength' => 1,
+//            'ajax' => [
+//                'url' => Url::to(['/geo/ajax/county']),
+//                'dataType' => 'json',
+//                'data' => new JsExpression('function(params) { return {q:params.term}; }'),
+//            ],
+//            'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+//            'templateResult' => new JsExpression('function(county) { return county.text; }'),
+//            'templateSelection' => new JsExpression('function (county) { return county.text; }'),
+//        ],
+//    ]); 
+    ?>
 
     <?= $form->field($model, 'city_id')->widget(\kartik\widgets\Select2::classname(), [
-        'data' => \yii\helpers\ArrayHelper::map(\derekisbusy\geo\models\GeoCity::find()->orderBy('id')->asArray()->all(), 'id', 'id'),
+//        'data' => \yii\helpers\ArrayHelper::map(\derekisbusy\geo\models\GeoCity::find()->orderBy('id')->asArray()->all(), 'id', 'id'),
         'options' => ['placeholder' => Yii::t('geo', 'Choose city')],
+        'initValueText' =>  $model->city_id ? GeoCity::findOne($model->city_id)->city : '' ,
         'pluginOptions' => [
-            'allowClear' => true
+            'allowClear' => true,
+            'minimumInputLength' => 1,
+            'ajax' => [
+                'url' => Url::to(['/geo/ajax/city']),
+                'dataType' => 'json',
+                'data' => new JsExpression('function(params) { return {q:params.term}; }'),
+            ],
+            'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+            'templateResult' => new JsExpression('function(city) { return city.text; }'),
+            'templateSelection' => new JsExpression('function (city) { return city.text; }'),
         ],
     ]); ?>
 
--- backend/modules/geo/views/zip/create.php
+++ backend/modules/geo/views/zip/create.php
@@ -13,8 +13,6 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="geo-zip-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
