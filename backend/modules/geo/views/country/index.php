3475ce82cecdbbb63be2eee8d78681be6718eb36
Derek Gifford <derekisbusy@gmail.com>
2017-02-26 17:30:51 -0500

    Switch GridView to DynaGrid.

--- backend/modules/geo/views/country/index.php
+++ backend/modules/geo/views/country/index.php
@@ -4,9 +4,21 @@
 /* @var $searchModel derekisbusy\geo\models\GeoCountrySearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-use yii\helpers\Html;
+
+use derekisbusy\geo\backend\modules\geo\assets\GeoCommonAsset;
+use derekisbusy\geo\models\GeoCity;
+use derekisbusy\geo\models\GeoCitySearch;
+use derekisbusy\geo\models\GeoState;
+use kartik\dropdown\DropdownX;
+use kartik\dynagrid\DynaGrid;
 use kartik\export\ExportMenu;
 use kartik\grid\GridView;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\web\JsExpression;
+use yii\widgets\Pjax;
+
+GeoCommonAsset::register($this);
 
 $this->title = Yii::t('geo', 'Geo Countries');
 $this->params['breadcrumbs'][] = $this->title;
@@ -15,47 +27,66 @@ $search = "$('.search-button').click(function(){
 	return false;
 });";
 $this->registerJs($search);
-?>
-<div class="geo-country-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+echo  \derekisbusy\growl\FlashGrowlWidget::widget();
 
-    <p>
-        <?= Html::a(Yii::t('geo', 'Create Geo Country'), ['create'], ['class' => 'btn btn-success']) ?>
-        <?= Html::a(Yii::t('geo', 'Advance Search'), '#', ['class' => 'btn btn-info search-button']) ?>
-    </p>
-    <div class="search-form" style="display:none">
-        <?=  $this->render('_search', ['model' => $searchModel]); ?>
-    </div>
-    <?php 
-    $gridColumn = [
-        ['class' => 'yii\grid\SerialColumn'],
-        ['attribute' => 'id', 'visible' => false],
-        'country_code',
-        'country',
-        [
-            'class' => 'yii\grid\ActionColumn',
-        ],
-    ]; 
-    ?>
-    <?= GridView::widget([
+$columns = [
+    [
+        'class'=>'kartik\grid\CheckboxColumn',
+        'headerOptions'=>['class'=>'kartik-sheet-style'],
+    ],
+    [
+        'class' => 'yii\grid\SerialColumn',
+        'visible' => false
+    ],
+    ['attribute' => 'id', 'visible' => false],
+    'country_code',
+    'country',
+    [
+        'class' => 'yii\grid\ActionColumn',
+    ],
+];
+
+
+echo Html::beginTag('div', ['class'=>'geo-country-index']);
+Pjax::begin();
+$gridId = 'geo-country-grid';
+echo DynaGrid::widget([
+    'columns' => $columns,
+    'options' => ['id' => 'geo-country-dynagrid'],
+    'allowThemeSetting' => false,
+    'gridOptions'=>[
+        'id' => $gridId,
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
-        'columns' => $gridColumn,
+        'responsive' => true,
         'pjax' => true,
         'pjaxSettings' => ['options' => ['id' => 'kv-pjax-container-geo-country']],
-        'panel' => [
-            'type' => GridView::TYPE_PRIMARY,
-            'heading' => '<span class="glyphicon glyphicon-book"></span>  ' . Html::encode($this->title),
+        'hover' => true,
+        'condensed' => true,
+        'floatHeader' => true,
+        'floatHeaderOptions' => [
+            'position' => 'absolute'
         ],
-        'export' => false,
-        // your toolbar can include the additional full export menu
+        'resizableColumns' => true,
+        'resizableColumnsOptions' => ['resizeFromBody' => true],
+        'persistResize' => true,
+        'hideResizeMobile' => true,
         'toolbar' => [
+            [
+                'content' => 
+                    Html::a('<i class="glyphicon glyphicon-repeat"></i>', ['index'], [
+                        'class' => 'btn btn-default', 
+                        'title' => Yii::t('backend', 'Reset')
+                    ]),
+            ],
+            '{dynagrid}',
+            '{dynagridFilter}',
+            '{dynagridSort}',
             '{export}',
             ExportMenu::widget([
                 'dataProvider' => $dataProvider,
-                'columns' => $gridColumn,
+                'columns' => ['id','country_code','country'],
                 'target' => ExportMenu::TARGET_BLANK,
                 'fontAwesome' => true,
                 'dropdownOptions' => [
@@ -68,8 +99,71 @@ $this->registerJs($search);
                 'exportConfig' => [
                     ExportMenu::FORMAT_PDF => false
                 ]
-            ]) ,
+            ]),
+            '{toggleData}'
         ],
-    ]); ?>
-
-</div>
+        'panel' => [
+            'heading' => '<h3 class="panel-title"><i class="glyphicon glyphicon-th-list"></i> ' . Html::encode($this->title) . ' </h3>',
+            'type' => 'info',
+            'before' => Html::a('<i class="glyphicon glyphicon-plus"></i> ' . Yii::t('geo', 'New Country'), ['create'], ['class' => 'btn btn-success']), 
+            'showFooter' => true,
+            'after' => 
+                Html::beginTag('div', ['class'=>' pull-left gridview-after-text']) .
+                Yii::t('geo', 'With selected: ').
+                Html::endTag('div') .
+                Html::a('<i class="glyphicon glyphicon-trash"></i> Delete', ['delete-multiple'], [
+                    'class' => 'pull-left clear btn btn-danger btn-delete-items',
+                    'data-confirm-message' => Yii::t('geo', 'Are you sure you want to delete these {item}?', ['item' => Yii::t('geo', 'countries')]),
+                    'data-grid' => $gridId,
+                    'data-csrf-param' => yii::$app->request->csrfParam,
+                    'data-csrf-token' => yii::$app->request->csrfToken
+                ]) .
+//                Html::beginTag('div', ['class'=>' pull-left dropdown', 'style' => 'margin-left:20px']) .
+//                Html::button('Status <span class="caret"></span></button>', 
+//                    ['type'=>'button', 'class'=>'btn btn-default', 'data-toggle'=>'dropdown']) . 
+//                DropdownX::widget([
+////                    'options'=>['class'=>'pull-right'], // for a right aligned dropdown menu
+//                    'items' => [
+//                        ['label' => Yii::t('geo','Active'), 'url' => 'javascript:;', 
+//                            'linkOptions' => [
+//                                'class' => 'btn-update-status',
+//                                'data-pjax-container' => '#geo-country-dynagrid-pjax',
+//                                'data-url' => Url::toRoute(['mark-multiple']),
+//                                'data-status' => GeoCountry::STATUS_ACTIVE,
+//                                'data-grid' => $gridId,
+//                                'data-csrf-param' => yii::$app->request->csrfParam,
+//                                'data-csrf-token' => yii::$app->request->csrfToken
+//                            ]
+//                        ],
+//                        ['label' => Yii::t('geo','Inactive'), 'url' => 'javascript:;', 
+//                            'linkOptions' => [
+//                                'class' => 'btn-update-status',
+//                                'data-pjax-container' => '#geo-city-dynagrid-pjax',
+//                                'data-url' => Url::toRoute(['mark-multiple']),
+//                                'data-status' => GeoCity::STATUS_INACTIVE,
+//                                'data-grid' => $gridId,
+//                                'data-csrf-param' => yii::$app->request->csrfParam,
+//                                'data-csrf-token' => yii::$app->request->csrfToken
+//                            ]
+//                        ],
+//                        ['label' => Yii::t('geo','Deleted'), 'url' => 'javascript:;', 
+//                            'linkOptions' => [
+//                                'class' => 'btn-update-status',
+//                                'data-pjax-container' => '#geo-city-dynagrid-pjax',
+//                                'data-url' => Url::toRoute(['mark-multiple']),
+//                                'data-status' => GeoCity::STATUS_DELETED,
+//                                'data-grid' => $gridId,
+//                                'data-csrf-param' => yii::$app->request->csrfParam,
+//                                'data-csrf-token' => yii::$app->request->csrfToken
+//                            ]
+//                        ],
+//                    ],
+//                ]) .
+//                Html::endTag('div') . 
+                Html::beginTag('div', ['class'=>' clearfix']) .
+                Html::endTag('div') 
+        ],
+    ],
+]);
+Pjax::end();
+echo Html::endTag('div');
