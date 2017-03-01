3df55c2c9da2594f3f698549f5d4a4b923e70a82
Derek Gifford <derekisbusy@gmail.com>
2017-02-28 19:03:08 -0500

    Add status column to state

---
+++ migrations/m170225_180102_alter_table_geo_state_add_column_status.php
@@ -0,0 +1,25 @@
+<?php
+
+use derekisbusy\geo\models\base\GeoState;
+
+class m170225_180102_alter_table_geo_state_add_column_status extends \yii\db\Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+        
+        $this->addColumn(
+                GeoState::tableName(), 
+                'status', 
+                $this->integer(1)->notNull()->defaultValue(1)->after('adjective'));
+                
+    }
+
+    public function down()
+    {
+        $this->dropColumn(GeoState::tableName(), 'status');
+    }
+}
