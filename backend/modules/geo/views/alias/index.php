f6274aa41500e669898a43ee23d823e273a46e15
Derek Gifford <derekisbusy@gmail.com>
2017-02-28 19:01:20 -0500

    Switch gridview to dynagrid

--- backend/modules/geo/views/alias/index.php
+++ backend/modules/geo/views/alias/index.php
@@ -4,58 +4,160 @@
 /* @var $searchModel derekisbusy\geo\models\GeoCityAliasSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-use yii\helpers\Html;
+use derekisbusy\geo\backend\modules\geo\assets\GeoCommonAsset;
+use derekisbusy\geo\models\GeoCityAlias;
+use derekisbusy\geo\models\GeoCity;
+use derekisbusy\geo\models\GeoState;
+use kartik\dropdown\DropdownX;
+use kartik\dynagrid\DynaGrid;
 use kartik\export\ExportMenu;
 use kartik\grid\GridView;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\web\JsExpression;
+use yii\widgets\Pjax;
 
-$this->title = Yii::t('geo', 'Geo City Aliases');
+GeoCommonAsset::register($this);
+
+$this->title = Yii::t('geo', 'City Aliases');
 $this->params['breadcrumbs'][] = $this->title;
 $search = "$('.search-button').click(function(){
 	$('.search-form').toggle(1000);
 	return false;
 });";
 $this->registerJs($search);
-?>
-<div class="geo-city-alias-index">
+echo  \derekisbusy\growl\FlashGrowlWidget::widget();
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a(Yii::t('geo', 'Create Geo City Alias'), ['create'], ['class' => 'btn btn-success']) ?>
-        <?= Html::a(Yii::t('geo', 'Advance Search'), '#', ['class' => 'btn btn-info search-button']) ?>
-    </p>
-    <div class="search-form" style="display:none">
-        <?=  $this->render('_search', ['model' => $searchModel]); ?>
-    </div>
-    <?php 
-    $gridColumn = [
-        ['class' => 'yii\grid\SerialColumn'],
-        ['attribute' => 'id', 'visible' => false],
-        'alias',
-        'city_id',
-        [
-            'class' => 'yii\grid\ActionColumn',
+$columns = [
+    [
+        'class'=>'kartik\grid\CheckboxColumn',
+        'headerOptions'=>['class'=>'kartik-sheet-style'],
+    ],
+    [
+        'class' => 'yii\grid\SerialColumn',
+        'visible' => false
+    ],
+    ['attribute' => 'id', 'visible' => false],
+    'alias',
+    [
+        'attribute' => 'city',
+        'label' => Yii::t('geo', 'City'),
+        'value' => function($model){
+            return $model->city;
+        },
+        'filterType' => GridView::FILTER_SELECT2,
+        'filterWidgetOptions' => [
+            'initValueText' =>  $searchModel->city ? \derekisbusy\geo\models\GeoCity::findOne($searchModel->city)->city : '' ,
+            'pluginOptions' => [
+                'allowClear' => true,
+                'minimumInputLength' => 1,
+                'ajax' => [
+                    'url' => Url::to(['/geo/ajax/city']),
+                    'dataType' => 'json',
+                    'data' => new JsExpression('function(params) { return {q:params.term}; }'),
+                ],
+                'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+                'templateResult' => new JsExpression('function(city) { return city.text; }'),
+                'templateSelection' => new JsExpression('function (city) { return city.text; }'),
+            ],
         ],
-    ]; 
-    ?>
-    <?= GridView::widget([
+        'filterInputOptions' => ['placeholder' => 'city', 'id' => 'grid-geo-zip-search-city_id']
+    ],
+    [
+        'attribute' => 'county',
+        'label' => Yii::t('geo', 'County'),
+        'value' => function($model){
+            return $model->county;
+        },
+        'filterType' => GridView::FILTER_SELECT2,
+        'filterWidgetOptions' => [
+            'initValueText' =>  $searchModel->county ? \derekisbusy\geo\models\GeoCounty::findOne($searchModel->county)->county : '' ,
+            'pluginOptions' => [
+                'allowClear' => true,
+                'minimumInputLength' => $searchModel->state ? 0 : 1,
+                'ajax' => [
+                    'url' => Url::to(['/geo/ajax/county?state='.$searchModel->state]),
+                    'dataType' => 'json',
+                    'data' => new JsExpression('function(params) { return {q:params.term}; }'),
+                ],
+                'escapeMarkup' => new JsExpression('function (markup) { return markup; }'),
+                'templateResult' => new JsExpression('function(county) { return county.text; }'),
+                'templateSelection' => new JsExpression('function (county) { return county.text; }'),
+            ],
+        ],
+        'filterInputOptions' => ['placeholder' => 'county', 'id' => 'grid-geo-city-search-county_id']
+    ],
+    [
+        'attribute' => 'state',
+        'value' => 'city.state.state',
+        'label' => Yii::t('geo', 'State'),
+//        'value' => function($model){
+//            return $model->state->id;
+//        },
+        'filterType' => GridView::FILTER_SELECT2,
+        'filter' => \yii\helpers\ArrayHelper::map(GeoState::find()->asArray()->all(), 'id', 'state'),
+        'filterWidgetOptions' => [
+            'pluginOptions' => ['allowClear' => true],
+        ],
+        'filterInputOptions' => ['placeholder' => 'State', 'id' => 'grid-geo-alias-search-state_id']
+    ],
+    [
+        'attribute' => 'status',
+        'value' => function($model){
+            return '<span class="label label-'.GeoCityAlias::getStatusLabelTypes()[$model->status].'">' . GeoCityAlias::getStatusOptions()[$model->status] . '</span>';
+        },
+        'format' => 'raw',
+        'filterType' => GridView::FILTER_SELECT2,
+        'filter' => GeoCityAlias::getStatusOptions(),
+        'filterWidgetOptions' => [
+            'pluginOptions' => ['allowClear' => true],
+        ],
+        'filterInputOptions' => ['placeholder' => 'Status', 'id' => 'grid-geo-alias-search-status']
+    ],
+    [
+        'class' => 'yii\grid\ActionColumn',
+    ],
+];
+
+
+echo Html::beginTag('div', ['class'=>'geo-alias-index']);
+$gridId = 'geo-alias-grid';
+echo DynaGrid::widget([
+    'columns' => $columns,
+    'options' => ['id' => 'geo-alias-dynagrid'],
+    'allowThemeSetting' => false,
+    'gridOptions'=>[
+        'id' => $gridId,
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
-        'columns' => $gridColumn,
+        'responsive' => true,
         'pjax' => true,
-        'pjaxSettings' => ['options' => ['id' => 'kv-pjax-container-geo-city-alias']],
-        'panel' => [
-            'type' => GridView::TYPE_PRIMARY,
-            'heading' => '<span class="glyphicon glyphicon-book"></span>  ' . Html::encode($this->title),
+        'pjaxSettings' => ['options' => ['id' => 'kv-pjax-container-geo-alias']],
+        'hover' => true,
+        'condensed' => true,
+        'floatHeader' => true,
+        'floatHeaderOptions' => [
+            'position' => 'absolute'
         ],
-        'export' => false,
-        // your toolbar can include the additional full export menu
+        'resizableColumns' => true,
+        'resizableColumnsOptions' => ['resizeFromBody' => true],
+        'persistResize' => true,
+        'hideResizeMobile' => true,
         'toolbar' => [
+            [
+                'content' => 
+                    Html::a('<i class="glyphicon glyphicon-repeat"></i>', ['index'], [
+                        'class' => 'btn btn-default', 
+                        'title' => Yii::t('backend', 'Reset')
+                    ]),
+            ],
+            '{dynagrid}',
+            '{dynagridFilter}',
+            '{dynagridSort}',
             '{export}',
             ExportMenu::widget([
                 'dataProvider' => $dataProvider,
-                'columns' => $gridColumn,
+                'columns' => ['id','alias','city_id'],
                 'target' => ExportMenu::TARGET_BLANK,
                 'fontAwesome' => true,
                 'dropdownOptions' => [
@@ -68,8 +170,69 @@ $this->registerJs($search);
                 'exportConfig' => [
                     ExportMenu::FORMAT_PDF => false
                 ]
-            ]) ,
+            ]),
+            '{toggleData}'
         ],
-    ]); ?>
-
-</div>
+        'panel' => [
+            'heading' => '<h3 class="panel-title"><i class="glyphicon glyphicon-th-list"></i> ' . Html::encode($this->title) . ' </h3>',
+            'type' => 'info',
+            'before' => Html::a('<i class="glyphicon glyphicon-plus"></i> ' . Yii::t('geo', 'New Alias'), ['create'], ['class' => 'btn btn-success']), 
+            'showFooter' => true,
+            'after' => 
+                Html::beginTag('div', ['class'=>' pull-left gridview-after-text']) .
+                Yii::t('geo', 'With selected: ').
+                Html::endTag('div') .
+                Html::a('<i class="glyphicon glyphicon-trash"></i> Delete', ['delete-multiple'], [
+                    'class' => 'pull-left clear btn btn-danger btn-delete-items',
+                    'data-confirm-message' => Yii::t('geo', 'Are you sure you want to delete these {item}?', ['item' => Yii::t('geo', 'aliases')]),
+                    'data-grid' => $gridId,
+                    'data-csrf-param' => yii::$app->request->csrfParam,
+                    'data-csrf-token' => yii::$app->request->csrfToken
+                ]) .
+                Html::beginTag('div', ['class'=>' pull-left dropdown', 'style' => 'margin-left:20px']) .
+                Html::button('Status <span class="caret"></span></button>', 
+                    ['type'=>'button', 'class'=>'btn btn-default', 'data-toggle'=>'dropdown']) . 
+                DropdownX::widget([
+                    'items' => [
+                        ['label' => Yii::t('geo','Active'), 'url' => 'javascript:;', 
+                            'linkOptions' => [
+                                'class' => 'btn-update-status',
+                                'data-pjax-container' => '#geo-alias-dynagrid-pjax',
+                                'data-url' => Url::toRoute(['mark-multiple']),
+                                'data-status' => GeoCity::STATUS_ACTIVE,
+                                'data-grid' => $gridId,
+                                'data-csrf-param' => yii::$app->request->csrfParam,
+                                'data-csrf-token' => yii::$app->request->csrfToken
+                            ]
+                        ],
+                        ['label' => Yii::t('geo','Inactive'), 'url' => 'javascript:;', 
+                            'linkOptions' => [
+                                'class' => 'btn-update-status',
+                                'data-pjax-container' => '#geo-alias-dynagrid-pjax',
+                                'data-url' => Url::toRoute(['mark-multiple']),
+                                'data-status' => GeoCity::STATUS_INACTIVE,
+                                'data-grid' => $gridId,
+                                'data-csrf-param' => yii::$app->request->csrfParam,
+                                'data-csrf-token' => yii::$app->request->csrfToken
+                            ]
+                        ],
+                        ['label' => Yii::t('geo','Deleted'), 'url' => 'javascript:;', 
+                            'linkOptions' => [
+                                'class' => 'btn-update-status',
+                                'data-pjax-container' => '#geo-alias-dynagrid-pjax',
+                                'data-url' => Url::toRoute(['mark-multiple']),
+                                'data-status' => GeoCity::STATUS_DELETED,
+                                'data-grid' => $gridId,
+                                'data-csrf-param' => yii::$app->request->csrfParam,
+                                'data-csrf-token' => yii::$app->request->csrfToken
+                            ]
+                        ],
+                    ],
+                ]) .
+                Html::endTag('div') . 
+                Html::beginTag('div', ['class'=>' clearfix']) .
+                Html::endTag('div') 
+        ],
+    ],
+]);
+echo Html::endTag('div');
