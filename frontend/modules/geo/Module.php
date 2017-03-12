f2f031111f46ba812ccbcaee67fca57bc4701233
Derek Gifford <derekisbusy@gmail.com>
2017-03-12 09:27:11 -0400

    Add module view settings

--- frontend/modules/geo/Module.php
+++ frontend/modules/geo/Module.php
@@ -7,6 +7,14 @@ namespace derekisbusy\geo\frontend\modules\geo;
  */
 class Module extends \derekisbusy\geo\BaseModule
 {
+    
+    const VIEW_INDEX = 1;
+    const VIEW_STATE = 2;
+    const VIEW_COUNTY = 3;
+    const VIEW_CITY = 4;
+    
+    public $viewSettings = [];
+    
     /**
      * @inheritdoc
      */
@@ -19,5 +27,11 @@ class Module extends \derekisbusy\geo\BaseModule
     {
         parent::init();
 
+        $this->viewSettings = array_replace_recursive([
+            self::VIEW_INDEX => 'index',
+            self::VIEW_STATE => 'state',
+            self::VIEW_COUNTY => 'county',
+            self::VIEW_CITY => 'city'
+        ], $this->viewSettings);
     }
 }
